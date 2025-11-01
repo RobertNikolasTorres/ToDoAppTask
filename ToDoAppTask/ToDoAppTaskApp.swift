5a57fc75afda053c09704c4490854615b8c5ca13
robert <robert.nikolas.torres@gmail.com>
2025-11-01 15:46:17 -0400

    Initial Commit

---
+++ ToDoAppTask/ToDoAppTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppTaskApp.swift
+//  ToDoAppTask
+//
+//  Created by Robert Nikolas Torres on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
